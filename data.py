958613bc0c261ecc12c129bf1728b08673063982
Aleksandr Dotsu <51963885+AlexDotu@users.noreply.github.com>
2024-10-29 09:31:56 +0100

    password and mail hidden

--- data.py
+++ data.py
@@ -4,5 +4,5 @@ datart_password = "qwerty"
 datart_incorrect_login = "qwerty"
 datart_incorrect_password = "qwerty"
 
-my_datart_login = "alexandrudotu@gmail.com"
-my_datart_password = "dotal777"
+my_datart_login = "alex********@gmail.com"
+my_datart_password = "do*****" # Пароль и емэйл скрыты
